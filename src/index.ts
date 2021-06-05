6cad77a19d41e797078a91231c661237fb203ae2
Shubham Singh Chahar <thefallenmerc@gmail.com>
2021-06-05 23:48:27 +0530

    added typescript capability

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express, { Application, Request, Response, NextFunction } from 'express';
+
+const app: Application = express();
+
+const port = 5000;
+
+app.use('/', (req: Request, res: Response, next: NextFunction) => {
+    return res.status(200).send({ data: "Hello" });
+});
+
+app.listen(port, () => {
+    console.log("Server running at " + port)
+});
